551da2c2dfaac667971a893d601c87e8381694ad
Mark Bell <mcbell@illinois.edu>
2016-12-08 18:30:08 +0000

    Updated test.

--- pyparma_test.py
+++ pyparma_test.py
@@ -10,4 +10,13 @@ A = [
 	# [0,-1,0]
 	]
 P = Polyhedron(hrep=intize(A))  # Build Polyhedron corresponding to A.x + b >= 0.
-print(P.poly.affine_dimension())  # Print dimension of Polyhedron.
+B = [
+	# Define positive orthant:
+	[0,1,0],
+	[0,0,1],
+	# Define planes:
+	[0,-1,0]
+	]
+Q = Polyhedron(hrep=intize(B))  # Build Polyhedron corresponding to A.x + b >= 0.
+print(Q.poly.affine_dimension())  # Print dimension of Polyhedron.
+print(P.poly == Q.poly)
