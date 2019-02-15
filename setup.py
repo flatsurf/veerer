c2a124ba097905fcac9bef06f158f1f29833b972
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2019-02-15 09:49:12 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,39 @@
+# -*- coding: utf-8 -*-
 import os
+import codecs
 from distutils.core import setup
+from distutils.cmd import Command
 
-with open(os.path.join(os.path.dirname(__file__), 'README')) as f:
-    README = f.read()
+def readfile(filename):
+    with codecs.open(filename,  encoding='utf-8') as f:
+        return f.read()
+
+class TestCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import subprocess
+
+        if subprocess.call(['sage', '-t', '--force-lib', 'veerer/']):
+            raise SystemExit("Doctest failures")
+
 
 setup(
     name='veerer',
-    version='0.0.1.beta',
+    version=readfile("VERSION"),
     description='A Python module to manipulate Veering triangulations and their associated flat structures',
-    long_description=README,
+    long_description=readfile("README"),
     author='Mark Bell, Vincent Delecroix and Saul Schleimer',
     author_email='vincent.delecroix@u-bordeaux.fr',
     url='https://framagit.org/saulsch/Veerer',
     # Remember to update these if the directory structure changes.
     packages=['veerer/'],
-#    install_requires=[
-#        'pplpy',
-#        'surface_dynamics'
-#        ],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
@@ -27,5 +43,5 @@ setup(
         'Programming Language :: Python',
         'Topic :: Scientific/Engineering :: Mathematics',
         ],
+    cmdclass = {'test': TestCommand}
 )
-
